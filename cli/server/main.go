6247bba2a3feb6dd8d582d82c42ec47270c1928d
dakugame <39842216+dakugame@users.noreply.github.com>
2021-03-10 09:43:39 -0500

    Update main.go

--- cli/server/main.go
+++ cli/server/main.go
@@ -19,21 +19,10 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Hello KARO!") // send data to client side
 }
-func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //get request method
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("login.gtpl")
-		t.Execute(w, nil)
-	} else {
-		r.ParseForm()
-		// logic part of log in
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
-	}
+
 }
 
 func main() {
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/login", login)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
